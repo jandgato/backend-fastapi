79fd3b4accc1a2deed9814122a0f0ea0dd59b6b1
Andres Garcia <garcia.andres.qb@gmail.com>
2022-12-30 16:19:09 -0600

    Work on main.py POST PUT DELETE

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 app.title = 'FastAPI Portfolio'
 app.version = '0.0.1'
+
+class Ruta(BaseModel):
+    id: Optional[str] = None
+    salida: str = Field(max_length=25)
+    llega: str = Field(max_length=25)
 
 rutas = [
     {
@@ -40,3 +47,33 @@ def get_ruta_by_salida(salida: str):
 def get_ruta_by_llega(llega: str):
     ruta = list(filter(lambda x: x['llega'] == llega,rutas))
     return ruta if len(ruta) > 0 else 'No existe la ruta'
+
+@app.post('/rutas/nueva/', tags=['Rutas'])
+def post_ruta(ruta: Ruta):
+    rutas.append(ruta)
+    return rutas
+
+def obtener_ruta(id):
+    for ruta in rutas:
+        if ruta['id'] == id:
+            return ruta
+    return None
+
+@app.put('/rutas/{id}', tags=['Rutas'])
+def put_ruta(id: str, ruta: Ruta):
+    item = obtener_ruta(id)
+    if item is not None:
+        item['salida'] = ruta.salida
+        item['llega'] = ruta.llega
+        return rutas
+    else:
+        return 'No existe la Ruta'
+
+@app.delete('/rutas/{id}', tags=['Rutas'])
+def delete_ruta(id: str):
+    ruta = obtener_ruta(id)
+    if ruta is not None:
+        rutas.remove(ruta)
+        return rutas
+    else:
+        return 'No existe la Ruta'
